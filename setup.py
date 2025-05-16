5488450251e6d80e8a0152f603c391c96daebed5
arko-mohari <arkomohari@gmail.com>
2025-05-16 13:49:03 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="omp-predictor",
+    version="1.0.0",
+    description="Predict regioselectivity in Pd-catalyzed C-H activation using machine learning.",
+    author="Ayan Datta, Arko Mohari, Oshiya RA",
+    author_email="spad@iacs.res.in",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "omp_predictor": ["*.pkl"],
+    },
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "rdkit-pypi",
+        "joblib",
+    ],
+    entry_points={
+        "console_scripts": [
+            "omp=omp_predictor.omp:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
